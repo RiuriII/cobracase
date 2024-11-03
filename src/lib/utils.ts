71b67fa95a726ea436b3dade1bb9befc96df8a0a
RiuriII <riuriboneta@gmail.com>
2024-11-03 15:02:47 -0300

    chore(metadata): add favicon to project

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { Metadata } from "next";
+
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -13,3 +15,32 @@ export const formatPrice = (price: number) => {
 
   return formatter.format(price);
 };
+
+export function constructMetadata({
+  title = "CaseCobra - Custom high-quality phone case",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico"
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    icons,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image]
+    }
+  };
+}
